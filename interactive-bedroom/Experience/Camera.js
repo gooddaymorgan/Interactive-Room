8735679b2d6e9c4fde032aa796cf0bd59ac894e4
Morgan <morgan.em.day@gmail.com>
2023-03-09 16:54:59 -0600

    Added Experience , Camera, and Sizes

---
+++ interactive-bedroom/Experience/Camera.js
@@ -0,0 +1,53 @@
+import * as THREE from "three";
+import Experience from "./Experience";
+
+export default class Camera {
+    constructor() {
+        this.experience = new Experience();
+        this.sizes = this.experience.sizes;
+        this.scene = this.experience.scene;
+        this.canvas = this.experience.canvas;
+
+        this.createPerspectiveCamera();
+        this.createOrthographicCamera();
+    }
+
+    createPerspectiveCamera() {
+        this.perspectiveCamera = new THREE.PerspectiveCamera(
+            35,
+            this.aspect,
+            0.1,
+            1000
+        );
+        this.scene.add(this.perspectiveCamera);
+        this.perspectiveCamera.position.x = 29;
+        this.perspectiveCamera.position.y = 14;
+        this.perspectiveCamera.position.z = 12;
+    }
+
+    createOrthographicCamera() {
+        this.orthographicCamera = new THREE.OrthographicCamera(
+            (this.sizes.aspect * this.sizes.frustrum)/ 2,
+            (this.sizes.aspect * this.sizes.frustrum)/ 2,
+            this.sizes.frustrum / 2,
+            -this.sizes.frustrum / 2,
+            -100,
+            100
+        );
+        this.scene.add(this.orthographicCamera);
+    }
+
+    resize(){
+
+        //Updates Perspective Camera on Resize
+        this.perspectiveCamera.aspect = this.sizes.aspect;
+        this.perspectiveCamera.updateProjectionMatrix();
+
+        //updates Ortho Camera on Resize
+        this.orthographicCamera.left = (-this.sizes.aspect * this.sizes.frustrum)/ 2;
+        this.orthographicCamera.right = (this.sizes.aspect * this.sizes.frustrum)/ 2;
+        this.orthographicCamera.top= this.sizes.frustrum / 2;
+        this.orthographicCamera.bottom = -this.sizes.frustrum / 2;
+        this.orthographicCamera.updateProjectionMatrix();
+    }
+}
--- interactive-bedroom/Experience/Experience.js
+++ interactive-bedroom/Experience/Experience.js
@@ -1,32 +1,19 @@
 import * as THREE from "three";
 
+import Sizes from "./Utils/Sizes.js";
+import Camera from "./Camera.js";
+
 export default class Experience{
-    constructor(canvas){
-        this.canvas = canvas;
-        
-        const scene = new THREE.Scene();
-			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    static instance
+	constructor(canvas) {
 
-			const renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
+		if(Experience.instance) {
+			return Experience.instance
+		}
 
-			const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			const cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
-
-			camera.position.z = 5;
-
-			function animate() {
-				requestAnimationFrame( animate );
-
-				cube.rotation.x += 0.01;
-				cube.rotation.y += 0.01;
-
-				renderer.render( scene, camera );
-			}
-
-			animate();
-    }
-}
+		Experience.instance = this
+		this.canvas = canvas;
+		this.scene = new THREE.Scene();
+		this.sizes = new Sizes();
+		this.camera = new Camera();
+}}
